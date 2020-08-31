2909d13ae06423232e06d2569180d56afd5c1fac
Jacques Zwielich <moshhowl@gmail.com>
2020-08-31 14:06:24 -0700

    Add project files.

---
+++ HomeWork1/Part2.cs
@@ -0,0 +1,79 @@
+﻿
+Skip to content
+Pull requests
+Issues
+Marketplace
+Explore
+@MrPatchz
+Learn Git and GitHub without any code!
+
+Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.
+MrPatchz /
+CS-155-C-
+
+1
+0
+
+    0
+
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+
+
+/// Homework 1 Part  2
+/// File Name: MyFirstProgram-1.cs
+/// @author: Jacques Zwielich 
+/// Date:  August 27, 2020
+///
+/// Problem Statement: Ask the user to enter two numbers, calculate the sum of
+/// these two numbers, and display the sum to the screen
+/// 
+/// 
+/// Overall Plan:
+/// 1) Use Console WriteLines to create a pound sign... OUT OF POUND SIGNS!!!
+/// 
+
+
+using System;
+
+namespace HW1Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("             ###    ###      ");
+            Console.WriteLine("            ###    ###      ");
+            Console.WriteLine("           ###    ###       ");
+            Console.WriteLine("    #####################");
+            Console.WriteLine("   #####################");
+            Console.WriteLine("        ###    ###       ");
+            Console.WriteLine(" #####################");
+            Console.WriteLine("#####################");
+            Console.WriteLine("     ###    ###       ");
+            Console.WriteLine("    ###    ###       ");
+            Console.WriteLine("   ###    ###       ");
+        }
+    }
+}
+
+    © 2020 GitHub, Inc.
+    Terms
+    Privacy
+    Security
+    Status
+    Help
+
+    Contact GitHub
+    Pricing
+    API
+    Training
+    Blog
+    About
+
---
+++ HomeWork1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeWork1
+{
+    class Part1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
